584594abe7b0e3321cc6b1c3f57ff6b0d466ce20
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-03-06 21:20:38 -0800

    More work on the prototype:   * New hackers (circles) now arrive toward the center of the room   * Using a more global animation loop as opposed to callbacks on each obj   * Hackers now move so their center is on another hacker (shuffle)   * Test moving hacker 0 to the upper left

--- proto/hacker.js
+++ proto/hacker.js
@@ -1,46 +1,71 @@
 // hacker 'class'
-var Hacker = function(id, room, options){
-  this.id = id;
+var Hacker = function(room, hackers, options) {
+  var self = this;
+  this.id = hackers.length;
   this.room = room;
   // options
   if(!options) options = {};
-  this.x = options.x || 50;
-  this.y = options.y || 50;
+  this.x = safe_x(options.x || 50);
+  this.y = safe_y(options.y || 50);
   
   // attrs
   this.avatar = false;
-  this.living = true;
+  this.idling = false; // used for global animation
   
-  this.draw_avatar();
-  this.wire_events();
+  draw_avatar();
+  wire_events();
+  
+  // internal functions
+  //
+  function safe_x(x) {
+    var r = Hacker.radius + 5;
+    var rm = room;
+    var newx = x;
+    if (x + r > rm.width){ newx = x - r }
+    if (x - r <= 0){ newx = x + r }
+    return newx;
+  };
+  
+  function safe_y(y) {
+    var r = Hacker.radius + 5;
+    var rm = room;
+    var newy = y;
+    if (y + r > rm.height){ newy = y - r }
+    if (y - r <= 0){ newy = y + r }
+    return newy;
+  };
+  
+  function draw_avatar() {
+    var av = self.avatar = room.circle(self.x, self.y, Hacker.radius);
+    av.attr({fill: "#eee", "fill-opacity": 0, stroke: "#fff", "stroke-width": 2, "stroke-opacity": 0});
+    return self.avatar;
+  };
+  
+  function wire_events() {
+    self.avatar.click(function(e){
+      self.poke();
+    });
+  };
 }
 
 Hacker.radius = 10;
 Hacker.opacity = 0.5;
 
-Hacker.prototype.draw_avatar = function(){
-  var self = this;
-  var av = self.avatar = self.room.circle(self.x, self.y, Hacker.radius);
-  av.attr({fill: "#eee", "fill-opacity": 0, stroke: "#fff", "stroke-width": 2, "stroke-opacity": 0});
+Hacker.prototype.is_point_inside = function(x, y) {
+  cx = this.x;
+  cy = this.y;
+  return ((x - cx) * (x - cx) + (y - cy) * (y - cy)) < Hacker.radius * Hacker.radius;
 };
 
-Hacker.prototype.wire_events = function(){
-  var self = this;
-  self.avatar.click(function(e){
-    self.poke();
-  });
-};
-
-Hacker.prototype.arrive = function(){
+Hacker.prototype.arrive = function() {
   console.log('> new hacker arriving! (' + this.id + ')');
   var self = this;
   var av = self.avatar;
   av.animate({"fill-opacity": Hacker.opacity, "stroke-opacity": Hacker.opacity}, 1500);
-  self.live();
   return self.avatar;
 };
 
-Hacker.prototype.poke = function(){
+Hacker.prototype.poke = function() {
   console.log('> poked hacker ' + this.id + ' ...');
   var self = this;
   var av = self.avatar;
@@ -49,14 +74,29 @@ Hacker.prototype.poke = function(){
   av.animate({r: 30, "fill-opacity": 1}, 300, function(){ av.animate({r: Hacker.radius, "fill-opacity": Hacker.opacity}, 1500, "backOut"); self.living = true;});
 };
 
-Hacker.prototype.live = function(){
+Hacker.prototype.update = function() {
   var self = this;
-  setTimeout(function(){
+  var av = self.avatar;
+  var xshift = Math.floor(Math.random() * 41)-21
+  var yshift = Math.floor(Math.random() * 41)-21
+  
+  // idling is the little movement all the hackers do ... quivering with anticipation!
+  if (!self.idling) {
     randx = Math.random()*4;
     randy = Math.random()*4;
-    if (self.living){
-      self.avatar.animate({cy: randy + self.y, cx: randx + self.x, easing: '<>'}, 1000);
-    };
-    self.live();
-  }, 1000);
+    randa = 2000 - Math.random()*500; // randomize the animation time
+    self.idling = true;
+    av.animate({cy: randy + self.y, cx: randx + self.x, easing: '<>'}, randa, function(){self.idling = false;});
+
+    for (i = 0; i < hackers.length; i++) {
+      h = hackers[i];
+      if (self.is_point_inside(h.x, h.y) && h.id != self.id) {
+        console.log('>> the center of ' + self.id + ' is inside ' + h.id + ' - shifting ' + xshift + ' ' + yshift);
+        self.x = self.x + xshift;
+        self.y = self.y + yshift;
+      }
+    }
+  }
+  
+  
 };
